1ea096616852a9e199af9e28cddf81419d637507
Nicolas <nicolas.piplard@edu.itescia.fr>
2023-03-21 20:21:47 +0100

    force migrate

--- BackendLaravel/routes/web.php
+++ BackendLaravel/routes/web.php
@@ -19,7 +19,9 @@ Route::get('/', function () {
 });
 
 Route::get('/migrate', function () {
-    Artisan::call('migrate');
+    Artisan::call('migrate', [
+        '--force' => true
+    ]);
     return 'Migrated';
 });
 
